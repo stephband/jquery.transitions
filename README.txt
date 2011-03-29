d8d25eea5459adb657efe93951ec742f3cdfbdd4
stephband <stephband@googlemail.com>
2011-03-29 11:11:11 +0100

    Breaking changes. Replaces callback function with options object that can have callback and fallback functions.

--- README.txt
+++ README.txt
@@ -1,15 +1,17 @@
-Now that we have class based transitions in the browser, we can trigger simple animations by adding and removing classes instead of relying on jQuery's .animate(). There are some advantages to class based animation, not least of which is the fact that it progressively enhances normal classes, but we do lose some functionality that .animate() provides - notably callbacks on animation end, and the ability to animate to 'hide', where display: none; is applied at the end of the animation. These two methods address these problems.
+Now that we have class based transitions in the browser, we can trigger simple animations by adding and removing classes instead of relying on jQuery's .animate().
+There are some advantages to class based animation, not least of which is the fact that it progressively enhances normal classes, but we do lose some functionality that .animate() provides - notably callbacks on animation end, and the ability to animate to 'hide', where display: none; is applied at the end of the animation.
+These two methods address these problems.
 
+<h2>Methods</h2>
+.addTransitionClass( className, options )
+.removeTransitionClass( className, options )
 
-.addTransitionClass( className, callback )
+Adds or removes the class className to the node. In addition, the class 'transition' is added for the duration of the transition, allowing you to define styles before, during and after a transition.
+Where support for CSS transitions is not detected, .addTransitionClass() and .removeTransitionClass behave as .addClass() and .removeClass() respectively.
+The class 'transition' is not added, and the callback is called immediately after the className is applied.
 
-Adds the class className to the node, along with the class 'transition'. The class 'transition' is removed at the end of the transition, allowing you to define styles in CSS for before, during and after the transition.
-
-Where transition support is not detected, addTransitionClass() behaves as addClass(). The class 'transition' is not added, and the callback is called immediately.
-
-
-.removeTransitionClass( className, callback )
-
-Removes the class className to the node, and adds the class 'transition'. The class 'transition' is removed at the end of the transition, allowing you to define styles in CSS for before, during and after the transition.
-
-Where transition support is not detected, removeTransitionClass() behaves as removeClass(). The class 'transition' is not added, and the callback is called immediately.
+<h2>Options</h2>
+<dl>
+	<dt>callback</dt><dd><i>function</i> Called at the end of the CSS transition, or if CSS transition support is not detected, directly after className has been applied. Where fallback is defined, callback is passed to fallback as the first argument.<dd>
+	<dt>fallback</dt><dd><i>function</i> When CSS transition support is not detected and fallback is defined, it is called directly before className is applied, with callback as its first argument. Typically this allows you to define jQuery animations to replace the missing CSS transitions.<dd>
+</dl>
--- jquery.transitions.js
+++ jquery.transitions.js
@@ -1,6 +1,6 @@
 // jquery.transitions.js
 // 
-// 1.2
+// 1.3
 // 
 // Feature detects CSS transitions and provides a means to manage
 // transitions that start or end with un-transitionable properties
@@ -14,118 +14,138 @@
 // to use without forking your code.
 
 (function(jQuery){
-  var docElem = jQuery(document),
-      testElem = jQuery('<div/>').css({
-        position: 'absolute',
-        top: -200,
-        width: 100,
-        height: 100,
-        WebkitTransition: 'top 0.001s linear',
-        MozTransition: 'top 0.001s linear',
-        OTransition: 'top 0.001s linear',
-        transition: 'top 0.001s linear'
-      }),
-      transitionClass = 'transition',
-      timer;
-  
-  function end(e){
-    e.data.obj
-    .unbind( jQuery.support.cssTransitionEnd, end )
-    .removeClass( transitionClass );
-    
-    e.data.callback && e.data.callback();
-  }
-  
-  // jQuery plugins
-  
-  function addTransitionClass( classNames, fn ) {
-    // Add the transition class then force the
-    // browser to reflow by measuring something.
-    this
-    .addClass( transitionClass )
-    .width();
-    
-    this
-    .unbind(jQuery.support.cssTransitionEnd, end)
-    .bind(jQuery.support.cssTransitionEnd, { obj: this, callback: fn }, end)
-    .addClass( classNames );
-    
-    return this;
-  }
-  
-  function removeTransitionClass( classNames, fn ) {
-    // Add the transition class then force the
-    // browser to reflow by measuring something.
-    this
-    .addClass( transitionClass )
-    .width();
-    
-    this
-    .unbind(jQuery.support.cssTransitionEnd, end)
-    .bind(jQuery.support.cssTransitionEnd, { obj: this, callback: fn }, end)
-    .removeClass( classNames );
-    
-    return this;
-  }
-  
-  // Feature testing for transitionend event
-  
-  function removeTest(){
-    clearTimeout(timer);
-    timer = null;
-    testElem.remove();
-  }
-  
-  function transitionEnd(e){
-    // Get rid of the test element
-    removeTest();
-    
-    // Store flags in jQuery.support
-    jQuery.support.cssTransition = true;
-    jQuery.support.cssTransitionEnd = e.type;
-    
-    // Redefine addTransitionClass and removeTransitionClass
-    jQuery.fn.addTransitionClass = addTransitionClass;
-    jQuery.fn.removeTransitionClass = removeTransitionClass;
-    
-    // Stop listening for transitionend
-    docElem.unbind('transitionend webkitTransitionEnd oTransitionEnd', transitionEnd);
-  }
-  
-  // Use addClass() and removeClass() methods by default
-  jQuery.fn.addTransitionClass = function( classNames, fn ){
-    this.addClass( classNames );
-    fn && fn();
-    return this;
-  };
-  
-  jQuery.fn.removeTransitionClass = function( classNames, fn ){
-    this.removeClass( classNames );
-    fn && fn();
-    return this;
-  };
-  
-  docElem
-  .bind('transitionend webkitTransitionEnd oTransitionEnd', transitionEnd)
-  .ready(function(){
-    // Put the test element in the body
-    document.body.appendChild( testElem[0] );
-    
-    // Force the browser to reflow.
-    testElem.width();
-    
-    // Apply CSS to trigger a transition
-    testElem.css({ top: -300 });
-    
-    // Set a timeout for the transition test to finish, and if it does not,
-    // get rid of the test element. Opera requires a much greater delay
-    // than the time the transition should take, worryingly.
-    timer = setTimeout(function(){
-      removeTest();
-      
-      // Store flags in jQuery.support
-      jQuery.support.cssTransition = false;
-      jQuery.support.cssTransitionEnd = false;
-    }, 100);
-  });
+	var docElem = jQuery(document),
+			testElem = jQuery('<div/>').css({
+				position: 'absolute',
+				top: -200,
+				width: 100,
+				height: 100,
+				WebkitTransition: 'top 0.001s linear',
+				MozTransition: 'top 0.001s linear',
+				OTransition: 'top 0.001s linear',
+				transition: 'top 0.001s linear'
+			}),
+			transitionClass = 'transition',
+			timer;
+	
+	function end(e){
+		e.data.obj
+		.unbind( jQuery.support.cssTransitionEnd, end )
+		.removeClass( transitionClass );
+		
+		e.data.callback && e.data.callback();
+	}
+	
+	// jQuery plugins
+	
+	function addTransitionClass( classNames, fn ) {
+		// Add the transition class then force the
+		// browser to reflow by measuring something.
+		this
+		.addClass( transitionClass )
+		.width();
+		
+		this
+		.unbind(jQuery.support.cssTransitionEnd, end)
+		.bind(jQuery.support.cssTransitionEnd, { obj: this, callback: fn }, end)
+		.addClass( classNames );
+		
+		return this;
+	}
+	
+	function removeTransitionClass( classNames, fn ) {
+		// Add the transition class then force the
+		// browser to reflow by measuring something.
+		this
+		.addClass( transitionClass )
+		.width();
+		
+		this
+		.unbind(jQuery.support.cssTransitionEnd, end)
+		.bind(jQuery.support.cssTransitionEnd, { obj: this, callback: fn }, end)
+		.removeClass( classNames );
+		
+		return this;
+	}
+	
+	// Feature testing for transitionend event
+	
+	function removeTest(){
+		clearTimeout(timer);
+		timer = null;
+		testElem.remove();
+	}
+	
+	function transitionEnd(e){
+		// Get rid of the test element
+		removeTest();
+		
+		// Store flags in jQuery.support
+		jQuery.support.cssTransition = true;
+		jQuery.support.cssTransitionEnd = e.type;
+		
+		// Redefine addTransitionClass and removeTransitionClass
+		jQuery.fn.addTransitionClass = addTransitionClass;
+		jQuery.fn.removeTransitionClass = removeTransitionClass;
+		
+		// Stop listening for transitionend
+		docElem.unbind('transitionend webkitTransitionEnd oTransitionEnd', transitionEnd);
+	}
+	
+	// Use addClass() and removeClass() methods by default
+	jQuery.fn.addTransitionClass = function( classNames, options ){
+		var fallback = options.fallback,
+				callback = options.callback;
+		
+		if (fallback) {
+			fallback.call(this, callback);
+			this.addClass(classNames);
+		}
+		else {
+			this.addClass(classNames);
+			callback && callback.call(this);
+		}
+		
+		return this;
+	};
+	
+	jQuery.fn.removeTransitionClass = function( classNames, fn ){
+		var fallback = options.fallback,
+				callback = options.callback;
+		
+		if (fallback) {
+			fallback.call(this, callback);
+			this.removeClass(classNames);
+		}
+		else {
+			this.removeClass(classNames);
+			callback && callback.call(this);
+		}
+		
+		return this;
+	};
+	
+	docElem
+	.bind('transitionend webkitTransitionEnd oTransitionEnd', transitionEnd)
+	.ready(function(){
+		// Put the test element in the body
+		document.body.appendChild( testElem[0] );
+		
+		// Force the browser to reflow.
+		testElem.width();
+		
+		// Apply CSS to trigger a transition
+		testElem.css({ top: -300 });
+		
+		// Set a timeout for the transition test to finish, and if it does not,
+		// get rid of the test element. Opera requires a much greater delay
+		// than the time the transition should take, worryingly.
+		timer = setTimeout(function(){
+			removeTest();
+			
+			// Store flags in jQuery.support
+			jQuery.support.cssTransition = false;
+			jQuery.support.cssTransitionEnd = false;
+		}, 100);
+	});
 })(jQuery);
